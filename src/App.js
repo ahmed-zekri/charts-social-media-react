ba74ad2ba3b8617ba28be16c852fec82daf97e13
ahmed103 <ahmed.zekri.a@gmail.com>
2022-01-23 02:29:27 +0100

    Groups length

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import './App.css';
 
-import {useEffect} from "react";
 import BarCharts from "./components/BarCharts";
 import Header from "./components/Header";
-
+import {useEffect, useState} from "react";
+import {URL} from "./Constants";
 
 
 function App() {
+    const [groupLength, setGroupLength] = useState(2);
+    const [data, setData] = useState({});
+    const fetchData = async () => {
+        const response = await fetch(URL + '/engagement');
 
+        return await response.json();
+    };
+    useEffect(() => {
+        fetchData().then(data => {
+            setData(data);
+        });
+
+
+    }, [])
     return (
         <div className="bg-dark text-light">
             <Header title={'Engagements (Daily updated)'}/>
+
             <div className="App">
-                <BarCharts/>
+                <div className="row justify-content-center">
+                    <div className="col-md-3">
+                        <div className="form-group">
+                            <label htmlFor="exampleInputEmail1">Groups length</label>
+                            <input min="1" type="number" className="form-control" aria-describedby="Groups length"
+                                   defaultValue="3"
+                                   onKeyPress={(e) => e.preventDefault()}
+                                   placeholder="Enter Groups length" value={groupLength}
+                                   onChange={(e) => {
+
+                                       if (e.target.value !== '' && !Number.isNaN(parseInt(e.target.value) && parseInt(e.target.value) >= 1))
+
+                                           setGroupLength(parseInt(e.target.value))
+                                   }}/>
+
+
+                        </div>
+                    </div>
+                </div>
+                <BarCharts data={data} group_length={groupLength}/>
 
             </div>
         </div>
     );
 }
+
 
 export default App;
--- src/components/BarCharts.js
+++ src/components/BarCharts.js
@@ -11,46 +11,41 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 import sm_unicode from "../utils";
 
 
-
 // Drawing all the charts
-function BarCharts() {
-    const fetchData = async () => {
-        const response = await fetch(URL + '/engagement');
+function BarCharts({group_length,data}) {
+ 
+//grouping the data by groups of 3
+    const [by_groups_labels, setLabels_by_groups] = useState([]);
+    const [by_groups_dataset, set_dataset_by_groups] = useState([]);
+    
 
-        return await response.json();
-    };
-
-
-    const [by_3_labels, setLabels_by3] = useState([]);
-    const [by_3_dataset, setDataset_by3] = useState([]);
-
-
+// fetching the data
     useEffect(() => {
 
 
-        fetchData().then(data => {
+      
 
-            let labels_by_3 = [];
-            let dataset_by_3 = [];
+            let labels_by_groups = [];
+            let dataset_by_groups = [];
             Object.values(data).forEach(function (element, index) {
 
-                if (index % 3 === 0) {
-                    dataset_by_3.push([element])
-                    labels_by_3.push([Object.keys(data)[index]])
+                if (index % group_length === 0) {
+                    dataset_by_groups.push([element])
+                    labels_by_groups.push([Object.keys(data)[index]])
                 } else {
-                    dataset_by_3[Math.floor(index / 3)].push(element)
-                    labels_by_3[Math.floor(index / 3)].push((Object.keys(data)[index]))
+                    dataset_by_groups[Math.floor(index / group_length)].push(element)
+                    labels_by_groups[Math.floor(index / group_length)].push((Object.keys(data)[index]))
 
                 }
 
 
             });
-            setLabels_by3(labels_by_3);
-            setDataset_by3(dataset_by_3);
+            setLabels_by_groups(labels_by_groups);
+            set_dataset_by_groups(dataset_by_groups);
 
 
-        });
-    }, []);
+     
+    }, [data, group_length]);
 // Register the plugin to all charts:
     Chart.register(ChartDataLabels);
     Chart.defaults.font.family = ['"Font Awesome 5 Brands"', '"Font Awesome 5 Pro"'];
@@ -61,65 +56,66 @@ function BarCharts() {
     return (
 
         <>
-            <div className={`${by_3_labels.length===0?'loading':'loading bg-dark text-light'}`}>
-                <ThreeDots visible={by_3_labels.length === 0}/></div>
+            <div className={`${by_groups_labels.length === 0 ? 'loading' : 'loading bg-dark text-light'}`}>
+                <ThreeDots visible={by_groups_labels.length === 0}/></div>
             <div className="row  justify-content-center bg-dark text-light">
 
                 <div className="col-md ">
 
 
-                    {by_3_labels.map((element, index) => {
+                    {by_groups_labels.map((element, index) => {
                         return (<div key={index} className="card mt-5 p-5 bg-dark text-light">
 
                             <BarChart
 
-                              options={{scales: {
+                                options={{
+                                    scales: {
 
-                                      y: {
-                                          ticks: {
-                                              font: {
-                                                  size: 25,
-                                              }
-                                          },
-                                          grid: {borderWidth: 5, borderColor: "white"},
-
-
-                                      }
-                                      , x: {
-                                          grid: {borderWidth: 5, borderColor: "white"}
-
-                                      }
-
-                                  },
-                                  plugins: {
-                                      datalabels: {
-
-                                          display: true,
-                                          color: "white",
-                                          formatter:  function (value, context) {
-                                              let code = sm_unicode[context.dataset.label.toLowerCase()]
+                                        y: {
+                                            ticks: {
+                                                font: {
+                                                    size: 25,
+                                                }
+                                            },
+                                            grid: {borderWidth: 5, borderColor: "white"},
 
 
-                                              return "+"+value+"\n"+String.fromCharCode(parseInt(code, 16));
+                                        }
+                                        , x: {
+                                            grid: {borderWidth: 5, borderColor: "white"}
+
+                                        }
+
+                                    },
+                                    plugins: {
+                                        datalabels: {
+
+                                            display: true,
+                                            color: "white",
+                                            formatter: function (value, context) {
+                                                let code = sm_unicode[context.dataset.label.toLowerCase()]
 
 
-                              },
+                                                return "+" + value + "\n" + String.fromCharCode(parseInt(code, 16));
 
-                                          align: "start",
-                                          offset:-70
-                                      }
-                                  },
-                                  legend: {
-                                      display: false
-                                  }
-                              }}
+
+                                            },
+
+                                            align: "start",
+                                            offset: -70
+                                        }
+                                    },
+                                    legend: {
+                                        display: false
+                                    }
+                                }}
                                 data={{
                                     labels: element,
                                     datasets: [
                                         {
                                             id: 1,
                                             label: 'Facebook',
-                                            data: by_3_dataset[index],
+                                            data: by_groups_dataset[index],
                                             backgroundColor: 'blue',
                                         },
 
@@ -139,7 +135,7 @@ function BarCharts() {
 
         </>
     )
-        ;
+
 }
 
 export default BarCharts;
